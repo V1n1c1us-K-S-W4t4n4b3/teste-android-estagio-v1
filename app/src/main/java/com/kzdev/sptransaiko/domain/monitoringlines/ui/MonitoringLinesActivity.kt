243f03e6d80cda9d1ad757414e3b3b764472b28e
V1n1c1us-K-S-W4t4n4b3 <vinikswatanabe@outlook.com>
2024-07-28 23:39:50 -0300

    checking if there is access for the disabled

--- app/src/main/java/com/kzdev/sptransaiko/domain/monitoringlines/ui/MonitoringLinesActivity.kt
+++ app/src/main/java/com/kzdev/sptransaiko/domain/monitoringlines/ui/MonitoringLinesActivity.kt
@@ -102,21 +102,31 @@ class MonitoringLinesActivity : AppCompatActivity(), OnMapReadyCallback {
         Log.d("resposta lista", "Updating map with stops: $buss")
 
         if (isMapReady) {
-            val originalBitmap = BitmapFactory.decodeResource(resources, R.drawable.icon_buss_blue)
+            val originalBitmap = BitmapFactory.decodeResource(resources, R.drawable.icon_buss_red)
             val scaledBitmap = Bitmap.createScaledBitmap(originalBitmap, 100, 100, false)
             val customIcon = BitmapDescriptorFactory.fromBitmap(scaledBitmap)
+
+            val specialBitmap = BitmapFactory.decodeResource(resources, R.drawable.icon_buss_blue)
+            val specialScaledBitmap = Bitmap.createScaledBitmap(specialBitmap, 100, 100, false)
+            val specialIcon = BitmapDescriptorFactory.fromBitmap(specialScaledBitmap)
 
             if (!isInitialUpdate) {
                 val boundsBuilder = LatLngBounds.Builder()
                 buss.forEach { boundsBuilder.include(it) }
                 val bounds = boundsBuilder.build()
                 mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 100))
-                isInitialUpdate = true 
+                isInitialUpdate = true
             }
 
             for (i in buss.indices) {
                 val stop = buss[i]
                 val stopBuss = data.vs[i]
+
+                val icon = if (stopBuss.a) specialIcon else customIcon
+
+                val snippet =
+                    if (stopBuss.a) "Acessível para pessoas com deficiência"
+                    else "Não acessível para pessoas com deficiência"
 
                 val existingMarker = markersMap[stopBuss.p]
                 if (existingMarker != null) {
@@ -126,7 +136,8 @@ class MonitoringLinesActivity : AppCompatActivity(), OnMapReadyCallback {
                         MarkerOptions()
                             .position(stop)
                             .title("Onibus: ${stopBuss.p}")
-                            .icon(customIcon)
+                            .snippet(snippet)
+                            .icon(icon)
                     )
                     newMarker?.tag = stopBuss.p
                     if (newMarker != null) {
